896b7971542b213cdc7b5294d4f266a3841c0213
Vasyl Kartychak <68468261+smartguy-coder@users.noreply.github.com>
2024-04-07 19:51:37 +0300

    add base celery service (#7)

--- auto_app/settings/urls.py
+++ auto_app/settings/urls.py
@@ -5,6 +5,7 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, Spec
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/token/", include("auth.urls")),
+
     path("schema/", SpectacularAPIView.as_view(), name="schema"),
     path("docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
     path("redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
